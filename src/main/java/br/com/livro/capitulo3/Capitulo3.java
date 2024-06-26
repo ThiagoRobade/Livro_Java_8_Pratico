869bca7a55edf06e85f271131df86118fa6801a7
ThiagoRobade <robade@live.com>
2024-06-26 14:50:21 -0300

    Capitulo 3 finalizado

--- src/main/java/br/com/livro/capitulo3/Capitulo3.java
+++ src/main/java/br/com/livro/capitulo3/Capitulo3.java
@@ -1,4 +1,46 @@
 package br.com.livro.capitulo3;
 
-public class Capitulo3 {
+class Capitulo3 {
+
+    // Definição da interface Validador
+    interface Validador<T> {
+        boolean valida(T valor);
+    }
+
+    public static void main(String[] args) {
+        // Primeira Thread usando classe anônima
+        Runnable r = new Runnable() {
+            public void run() {
+                for (int i = 0; i <= 1000; i++) {
+                    System.out.println(i);
+                }
+            }
+        };
+        new Thread(r).start();
+
+        // Segunda Thread usando lambda
+        new Thread(() -> {
+            for (int i = 0; i <= 1000; i++) {
+                System.out.println(i);
+            }
+        }).start();
+
+        // Validador de CEP usando classe anônima
+        Validador<String> validadorCEP = new Validador<String>() {
+            public boolean valida(String valor) {
+                return valor.matches("[0-9]{5}-[0-9]{3}");
+            }
+        };
+
+        // Validador de CEP usando lambda
+        Validador<String> validadorCEPComLambda = valor -> valor.matches("[0-9]{5}-[0-9]{3}");
+
+        // Exemplo de uso do Runnable com lambda
+        Runnable o = () -> {
+            System.out.println("O que sou eu? Que lambda?");
+        };
+
+        System.out.println(o);
+        System.out.println(o.getClass());
+    }
 }
